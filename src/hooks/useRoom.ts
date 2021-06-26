f93fd54cc251b16bb5ee3207efa1461f5dddb814
RuthMaria <ruth-15rms@hotmail.com>
2021-06-26 18:03:17 -0300

    created 'admin room' page and funcionaly of like

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -12,6 +12,8 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 }
 
 type FirebaseQuestions = Record<string, {
@@ -47,14 +49,20 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered,}
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
+        }
       })
 
       setTitle(databaseRoom.title)
       setQuestions(parsedQuestions)
     })
 
-  }, [roomId])
+    return () => {
+      roomRef.off('value')
+    }
+  }, [roomId, user?.id])
 
   return { questions, title }
 }
